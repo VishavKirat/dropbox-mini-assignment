c410726ea91c0fc47bfa27b3ffc4b375491a1dc3
Vishav kirat Singh <vish_waraich@Vishavs-MacBook-Air.local>
2018-03-21 14:00:42 -0700

    first commit

---
+++ Post/Post.js
@@ -0,0 +1,30 @@
+'use strict';
+const AWS = require('aws-sdk'); 
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+module.exports.Post = (event, context, callback) => {
+  event.Records.forEach((record) => {
+    const fileName = record.s3.object.key;
+    const action = record.eventName
+    const Time = record.eventTime
+
+    let params = {
+      TableName: 'dropbox-minini',
+      Item: {
+        Id: uuid.v1(),
+        fileName: this.fileName,
+        Action: this.action,
+        Time: this.Time
+      }
+    }
+
+    dynamoDb.put(params, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+});
+};
