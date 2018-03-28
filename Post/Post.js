9be4c20f1941c15f82726814d529139f42ae7569
Vishav kirat Singh <vish_waraich@Vishavs-MacBook-Air.local>
2018-03-28 14:07:00 -0700

    firstCommit

--- Post/Post.js
+++ Post/Post.js
@@ -1,6 +1,12 @@
 'use strict';
 const AWS = require('aws-sdk'); 
 const uuid = require('uuid');
+const twilioaccountid = proceess.env.Twilio_SSID;
+const Twilio_Token = process.env.Twilio_Token;
+const Twilio_phoneNumber = process.env.Twilio_phoneNumber;
+const Calling_Number = process.env.Calling_Number;
+const twilio = require('twilio');
+const client = new twilio(Twilio_SSID, Twilio_Token);
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
@@ -19,6 +25,14 @@ module.exports.Post = (event, context, callback) => {
         Time: this.Time
       }
     }
+    client.messages.create({
+      to: Calling_Number,
+      from: Twilio_phoneNumber,
+      body:'hi you have added' + fileName + 'in your database';
+    },
+    (error,message) => {
+      window.alert(message);
+    })
 
     dynamoDb.put(params, (err, result) => {
       if (err) {
